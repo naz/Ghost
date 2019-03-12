e65199358560840944b5a5a1608a7f7820de1295
Fabien O'Carroll <fabien@allou.is>
2019-03-12 17:06:52 +0100

    Updated index.js to use es6
    no-issue

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 // # Ghost Startup
 // Orchestrates the startup of Ghost when run from command line.
 
-var startTime = Date.now(),
-    debug = require('ghost-ignition').debug('boot:index'),
-    ghost, express, common, urlService, parentApp;
+const startTime = Date.now;
+const debug = require('ghost-ignition').debug('boot:index');
 
 debug('First requires...');
 
-ghost = require('./core');
+const ghost = require('./core');
 
 debug('Required ghost');
 
-express = require('express');
-common = require('./core/server/lib/common');
-urlService = require('./core/server/services/url');
-parentApp = express();
+const express = require('express');
+const common = require('./core/server/lib/common');
+const urlService = require('./core/server/services/url');
+const parentApp = express();
 
 debug('Initialising Ghost');
-ghost().then(function (ghostServer) {
+ghost().then((ghostServer) => {
     // Mount our Ghost instance on our desired subdirectory path if it exists.
     parentApp.use(urlService.utils.getSubdir(), ghostServer.rootApp);
 
     debug('Starting Ghost');
     // Let Ghost handle starting our server instance.
     return ghostServer.start(parentApp)
-        .then(function afterStart() {
+        .then(() => {
             common.logging.info('Ghost boot', (Date.now() - startTime) / 1000 + 's');
         });
-}).catch(function (err) {
+}).catch((err) => {
     common.logging.error(err);
     setTimeout(() => {
         process.exit(-1);
