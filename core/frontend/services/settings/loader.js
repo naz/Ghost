4c024dce6ee0dda328b1580a3ec382943aeaeea0
Nazar Gargol <nazargargol@gmail.com>
2020-09-09 19:44:23 +1200

    Extended frontend settings loader with async loader
    - The default behavior of the loader is to load settings syncronously for reasons specified in https://github.com/TryGhost/Ghost/commit/0ac19dcf8419b20dd1005b6d88b44d55de2c1bff
    - To avoid blocking the eventloop added async loading method

--- core/frontend/services/settings/loader.js
+++ core/frontend/services/settings/loader.js
@@ -21,6 +21,39 @@ const getSettingFilePath = (setting) => {
 };
 
 /**
+ * Functionally same as loadSettingsSync with exception of loading
+ * settigs asyncronously. This method is used at new places to read settings
+ * to prevent blocking the eventloop
+ *
+ * @param {String} setting the requested settings as defined in setting knownSettings
+ * @returns {Object} settingsFile
+ */
+const loadSettings = async (setting) => {
+    const {fileName, contentPath, filePath} = getSettingFilePath(setting);
+
+    try {
+        const file = await fs.readFile(filePath, 'utf8');
+        debug('settings file found for', setting);
+
+        const object = yamlParser(file, fileName);
+        return validate(object);
+    } catch (err) {
+        if (errors.utils.isIgnitionError(err)) {
+            throw err;
+        }
+
+        throw new errors.GhostError({
+            message: i18n.t('errors.services.settings.loader', {
+                setting: setting,
+                path: contentPath
+            }),
+            context: filePath,
+            err: err
+        });
+    }
+};
+
+/**
  * Reads the desired settings YAML file and passes the
  * file to the YAML parser which then returns a JSON object.
  * NOTE: loading happens syncronously
@@ -52,3 +85,5 @@ module.exports = function loadSettingsSync(setting) {
         });
     }
 };
+
+module.exports.loadSettings = loadSettings;
